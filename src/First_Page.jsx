40a237672a9f011b35f1e472a26974ee48b2f546
Ziyan Zhai(Tom) <102450766+zhai1027@users.noreply.github.com>
2022-09-23 02:29:48 +1000

    Update First_Page.jsx

--- src/First_Page.jsx
+++ src/First_Page.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Main_Page.css'
-import Main from './Main_Page.jsx'
+import {Link} from 'react-router-dom';
 
 function Greeting(){
     return (
     <div className='Greeting' style={{marginTop: 200}}>
         <h1>Welcome to 7.1P</h1>
         <h2>From Ziyan Zhai</h2>
-        <a href='http://localhost:3000/home'>
-            <button type='button' onClick={<Main/>}>Jump to main page</button>
+        <Link to = '/login'></Link>
         </a>
     </div>
     )
